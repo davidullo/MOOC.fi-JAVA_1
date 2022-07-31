3757930ffff69e31b08da12970cf8e70ae8bbee9
davidullo <davbarbi@gmail.com>
2022-07-31 18:04:41 +0200

    exercises module 2

--- mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,20 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        double sum = 0;
+        double countNumbers = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 0) {
+                break;
+            } else {
+                sum += number;
+                countNumbers += 1;
+                continue;
+            }
+        }
+        double average = sum / countNumbers;
+        System.out.println("Average of the numbers: " + average);
     }
 }
--- mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,26 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        double countNumbers = 0;
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 0) {
+                break;
+            } else if (number > 0) {
+                sum += number;
+                countNumbers += 1;
+                continue;
+            } else {
+                continue;
 
+            }
+        }
+        double average = sum / countNumbers;
+        if (sum <= 0) {
+            System.out.println("Cannot calculate average");
+        } else {
+            System.out.println(average);
+        }
     }
 }
