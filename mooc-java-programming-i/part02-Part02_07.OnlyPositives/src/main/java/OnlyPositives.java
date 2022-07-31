9accbcc1c139c8bff3980d79c7277c9eecafd6fc
davidullo <davbarbi@gmail.com>
2022-07-31 17:25:58 +0200

    exercises module 2

--- mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -5,6 +5,19 @@ public class OnlyPositives {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int number = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            number = Integer.valueOf(scanner.nextLine());
+            if (number < 0){
+                System.out.println("Unsuitable number");
+            }
+            if (number == 0){
+                break;
+            }
+            if (number > 0){
+                System.out.println(number * number);
+            }
+        }
     }
 }
