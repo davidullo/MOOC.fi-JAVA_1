fa5189d6cd9ebd7ec188b991ddbce241540b15d1
davidullo <davbarbi@gmail.com>
2022-08-08 20:48:36 +0200

    module 4

--- mooc-java-programming-i/part04-Part04_17.Items/src/main/java/Items.java
+++ mooc-java-programming-i/part04-Part04_17.Items/src/main/java/Items.java
@@ -10,5 +10,24 @@ public class Items {
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true)
+        {
+            System.out.println("Name:");
+            String name = scanner.nextLine();
+            if (name.isEmpty())
+            {
+                break;
+            } else
+            {
+                items.add(new Item(name));
+            }
+        }
+
+        for (Item single : items)
+        {
+//            single.toString();
+            System.out.println(single);
+        }
+
     }
 }
