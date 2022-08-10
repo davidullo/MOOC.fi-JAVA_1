96ff038909d810f7024ce675ebcca450fffb6208
davidullo <davbarbi@gmail.com>
2022-08-10 17:57:28 +0200

    module 4

--- mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -9,6 +9,26 @@ public class PersonalInformationCollection {
 
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("First name:");
+            String first = scanner.nextLine();
+            if (first.isEmpty()) {
+                break;
+            }
+            System.out.println("Last name:");
+            String last = scanner.nextLine();
+            
+            System.out.println("Identification number:");
+            String idNum = scanner.nextLine();
+            
+            infoCollection.add(new PersonalInformation(first,last,idNum));
+        }
+        
+        
+        for (PersonalInformation person : infoCollection) {
+            System.out.println(person.getFirstName() + " " + person.getLastName());
+        }
 
     }
 }
