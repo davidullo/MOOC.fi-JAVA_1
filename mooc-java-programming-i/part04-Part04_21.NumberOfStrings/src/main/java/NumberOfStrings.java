34087cd408c442dc1d678d6ed2384402e33788ea
davidullo <davbarbi@gmail.com>
2022-08-14 11:59:16 +0200

    module 4

--- mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,15 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int count = 0;
+        String word = "";        
+        while (true) {
+            if (word.equals("end")) {
+                break;
+            }
+            word = scanner.nextLine();
+            count++;
+        }
+        System.out.println(count - 1);
     }
 }
--- mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,15 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (!(input.equals("end"))) {
+            int number = Integer.valueOf(input);
+            System.out.println(number * number * number);
+            } else {
+                break;
+            }
+        }
     }
 }
