bf584f71a8c3db02fe9f6f76a8119b3e29d16b9e
davidullo <davbarbi@gmail.com>
2022-08-17 13:21:44 +0200

    module 4

--- mooc-java-programming-i/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ mooc-java-programming-i/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -7,13 +7,35 @@ public class NumbersFromAFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("File? ");
+        System.out.println("File? ");
         String file = scanner.nextLine();
-        System.out.print("Lower bound? ");
+        System.out.println("Lower bound? ");
         int lowerBound = Integer.valueOf(scanner.nextLine());
-        System.out.print("Upper bound? ");
+        System.out.println("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
 
+        int count = 0;
+
+        try (Scanner fileReader = new Scanner(Paths.get(file)))
+        {
+            while (fileReader.hasNextLine())
+            {
+                int looked = Integer.valueOf(fileReader.nextLine());
+                if (looked < lowerBound || looked > upperBound)
+                {
+                    continue;
+                } else
+                {
+                    count++;
+                    continue;
+                }
+            }
+        } catch (Exception e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        System.out.println("Numbers: " + count);
     }
 
 }
